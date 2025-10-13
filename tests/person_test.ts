0a5e45c010a2e70d9fdb78135ae07e1d7514a14e
WasixXD <wasilewski2020@gmail.com>
2025-10-13 11:49:08 -0300

    no console

--- tests/person_test.ts
+++ tests/person_test.ts
@@ -154,7 +154,6 @@ Deno.test("person.cnh() does not return the same cnh", () => {
 Deno.test("person.cnpj()", () => {
   const cnpj = person.cnpj();
 
-  console.log(cnpj);
   assertEquals(cnpj.length, 18);
   assertStringIncludes(cnpj, "/");
   assertStringIncludes(cnpj, ".");
