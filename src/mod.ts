f24dd7f31dc430ce83663562f72220afd87b5fde
WasixXD <wasilewski2020@gmail.com>
2025-10-13 12:06:40 -0300

    feat: export symbols

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,8 @@
 import { Saci } from "./Saci.ts";
 
+export * from "./Saci.ts";
+export * from "./brasil/Brasil.ts";
+export * from "./person/Person.ts";
+export * from "./student/Student.ts";
+
 export const saci: Saci = new Saci();
